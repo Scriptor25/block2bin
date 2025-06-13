b8d5bc7324e1a096641b2c11fa9cabfbf718cfa7
Felix Schreiber <felix.schreiber@auctores.de>
2025-06-13 10:41:37 +0200

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+int main()
+{
+    // 1. extract source files and assets from project file
+    // 2. build ast from source json structure
+    // 3. compile assets to data blobs
+    // 4. compile ast to ir, use assets as global constants
+    // 5. write ir to file, or compile ir to object file, or run the ir using a jit runtime
+    // --> single file executable with all code and assets combined
+
+    // - how to display the game? --> maybe linked library with opengl and glfw
+    // --> scratch runtime library!!!
+    // --> whole scratch engine
+}
